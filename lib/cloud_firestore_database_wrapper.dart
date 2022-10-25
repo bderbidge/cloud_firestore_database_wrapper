19adfac88036ad81f5c3074f15f42184315bd1d0
bderbidge <brander81@gmail.com>
2022-10-24 20:26:29 -0600

    expose firestore parser

--- lib/cloud_firestore_database_wrapper.dart
+++ lib/cloud_firestore_database_wrapper.dart
@@ -3,3 +3,4 @@ library cloud_firestore_database_wrapper;
 export 'src/firestore_data_source.dart';
 export 'interfaces/i_data_source.dart';
 export 'src/base_model.dart';
+export 'util/firestore_parser.dart';
